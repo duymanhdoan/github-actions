6fd074d21cc9a025f361840d87e025eddce86946
duymanhdoan <doanmanhduy.yb0210@gmail.com>
2023-10-23 15:39:17 +0700

    fix syntax error

--- hello-github-actions.py
+++ hello-github-actions.py
@@ -7,5 +7,5 @@ def main():
     print("update code")
 
 if __name__ == "__main__":
-    print(wellcome to github actions !)
+    print(" build the code by github actions successfully !")
     main()
