560ce673ac4cf16a8c0d6ed32ab3fb18d66af79b
duymanhdoan <doanmanhduy.yb0210@gmail.com>
2023-10-23 12:05:03 +0700

    update code

--- hello-github-actions.py
+++ hello-github-actions.py
@@ -3,9 +3,9 @@ import os
 
 
 def main():
-    print(“What about you today manh duy ?”)
+    print("What about you today manh duy ?")
+    print("update code")
 
-
-if __name__ == “__main__”:
-    print(“wellcome to github actions !”)
+if __name__ == "__main__":
+    print(wellcome to github actions !)
     main()
